e70aca09252563998ead624bf4d8130248fca237
amberlin@biomdcare.com <amberlin@biomdcare.com>
2022-09-13 14:07:26 +0800

    拆出標準作業書

--- LDTS/StandarWorkbook.aspx.cs
+++ LDTS/StandarWorkbook.aspx.cs
@@ -18,12 +18,7 @@ namespace LDTS
                 Admin loginAdmin = (Admin)Session["LDTSAdmin"];
                 int SID = Convert.ToInt32(Request.QueryString["sid"]);
                 string Sname = StandarWorkBookService.GetAllStandarwookbooks().Where(x => x.SID == SID).Select(x => x.Sname).FirstOrDefault();
-                SwbName.Text = Sname;
-                sName.Text = Sname;
-                desc.Text = StandarWorkBookService.GetAllStandarwookbooks().Where(x => x.SID == SID).Select(x => x.Description).FirstOrDefault().ToString();
-                sIndex.Text = StandarWorkBookService.GetAllStandarwookbooks().Where(x => x.SID == SID).Select(x => x.Sindex).FirstOrDefault().ToString();
-                download.HRef = "Upload/" + StandarWorkBookService.GetAllStandarwookbooks().Where(x => x.SID == SID).Select(x => x.new_filename).FirstOrDefault().ToString();
-
+                title.InnerText = Sname;
                 List<int> reQids = Service.RelationService.GetAllreSWorkBookForm().Where(x => x.SID == SID).Select(x => x.QID).ToList();
                 List<ReportQuestion> reportQuestions = new List<ReportQuestion>();
                 List<ReportQuestion> Allreports = ReportQuestiovService.GetAllReportQuestions();
@@ -122,84 +117,5 @@ namespace LDTS
             }
         }
 
-        protected void SaveButton_Click(object sender, EventArgs e)
-        {
-            //修改
-            string id = Request.QueryString["sid"];
-            StandardWorkBook workBook = StandarWorkBookService.GetAllStandarwookbooks().Where(x => x.SID == Convert.ToInt32(id)).FirstOrDefault();
-            workBook.Sname = sName.Text;
-            workBook.Sindex =Convert.ToInt32( sIndex.Text);
-            workBook.Description = desc.Text;
-            if (StandarWorkBookService.UpdateStandarwookbook(workBook))
-            {
-                Admin loginAdmin = (Admin)Session["LDTSAdmin"];
-                string work = "編輯標準作業書:" + workBook.Sname;
-                LDTSservice.InsertRecord(loginAdmin, work);
-
-                Literal AlertMsg = new Literal();
-                AlertMsg.Text = "<script language='javascript'>alert('編輯成功!');</script>";
-                this.Page.Controls.Add(AlertMsg);
-            }
-            else
-            {
-                Literal AlertMsg = new Literal();
-                AlertMsg.Text = "<script language='javascript'>alert('編輯失敗!');</script>";
-                this.Page.Controls.Add(AlertMsg);
-
-            }
-        }
-
-        protected void uploadfile_Click(object sender, EventArgs e)
-        {
-            Literal AlertMsg = new Literal();
-            Admin admin = (Admin)Session["LDTSAdmin"];
-            int id = Convert.ToInt32(Request.QueryString["sid"]);
-
-            StandardWorkBook standardWorkBook = StandarWorkBookService.GetAllStandarwookbooks().Where(x => x.SID == id).FirstOrDefault();
-
-            //Update
-            if (standardWorkBook != null)
-            {
-                StandardWorkBook UpdatestandardWBk = new StandardWorkBook();
-                UpdatestandardWBk.old_filename = standardWorkBook.old_filename;
-                UpdatestandardWBk.new_filename = standardWorkBook.new_filename;
-                if (swbUpload.HasFile)
-                {
-                    string serverPath = Server.MapPath("~/Upload/");
-                    string fileName = swbUpload.FileName;
-                    string fileType = System.IO.Path.GetExtension(fileName);
-                    if (fileType.Contains(".docx") || fileType.Contains(".doc") || fileType.Contains(".pdf"))
-                    {
-                        UpdatestandardWBk.old_filename = fileName;
-                        string now = DateTime.Now.ToString("yyyyMdHmm");
-                        UpdatestandardWBk.new_filename = now + "_" + fileName;
-                        serverPath = serverPath + UpdatestandardWBk.new_filename;
-                        swbUpload.SaveAs(serverPath);
-                    }
-                    else
-                    {
-                        AlertMsg.Text = "<script language='javascript'>alert( '檔案格式錯誤!請上傳doc檔、docx檔或pdf檔。');</script>";
-                        this.Page.Controls.Add(AlertMsg);
-                        return;
-                    }
-                }
-                //基本資料
-                UpdatestandardWBk.SID = standardWorkBook.SID;
-                UpdatestandardWBk.Sname = sName.Text;
-                UpdatestandardWBk.Sindex = Convert.ToInt32(sIndex.Text);
-                UpdatestandardWBk.Description = desc.ToString();
-                bool isUpdate = StandarWorkBookService.UpdateStandarwookbook(UpdatestandardWBk);
-                if (!isUpdate)
-                {
-                    AlertMsg.Text = "<script language='javascript'>alert('更換標準作業書檔案失敗!');</script>";
-                    this.Page.Controls.Add(AlertMsg);
-                    return;
-                }
-                LDTSservice.InsertRecord(admin, "更換標準作業書檔案:" + sName.Text);
-                AlertMsg.Text = "<script language='javascript'>alert('更換標準作業書檔案成功!');</script>";
-                this.Page.Controls.Add(AlertMsg);
-            }
-
-        }
     }
 }
---
+++ LDTS/StandarWorkbookEdit.aspx.designer.cs
@@ -0,0 +1,125 @@
+﻿//------------------------------------------------------------------------------
+// <自動產生的>
+//     這段程式碼是由工具產生的。
+//
+//     變更這個檔案可能會導致不正確的行為，而且如果已重新產生
+//     程式碼，則會遺失變更。
+// </自動產生的>
+//------------------------------------------------------------------------------
+
+namespace LDTS
+{
+
+
+    public partial class StandarWorkbookEdit
+    {
+
+        /// <summary>
+        /// SwbName 控制項。
+        /// </summary>
+        /// <remarks>
+        /// 自動產生的欄位。
+        /// 若要修改，請將欄位宣告從設計工具檔案移到程式碼後置檔案。
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label SwbName;
+
+        /// <summary>
+        /// sIndex 控制項。
+        /// </summary>
+        /// <remarks>
+        /// 自動產生的欄位。
+        /// 若要修改，請將欄位宣告從設計工具檔案移到程式碼後置檔案。
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox sIndex;
+
+        /// <summary>
+        /// sName 控制項。
+        /// </summary>
+        /// <remarks>
+        /// 自動產生的欄位。
+        /// 若要修改，請將欄位宣告從設計工具檔案移到程式碼後置檔案。
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox sName;
+
+        /// <summary>
+        /// desc 控制項。
+        /// </summary>
+        /// <remarks>
+        /// 自動產生的欄位。
+        /// 若要修改，請將欄位宣告從設計工具檔案移到程式碼後置檔案。
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox desc;
+
+        /// <summary>
+        /// TemplateFile 控制項。
+        /// </summary>
+        /// <remarks>
+        /// 自動產生的欄位。
+        /// 若要修改，請將欄位宣告從設計工具檔案移到程式碼後置檔案。
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.HiddenField TemplateFile;
+
+        /// <summary>
+        /// swbUpload 控制項。
+        /// </summary>
+        /// <remarks>
+        /// 自動產生的欄位。
+        /// 若要修改，請將欄位宣告從設計工具檔案移到程式碼後置檔案。
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.FileUpload swbUpload;
+
+        /// <summary>
+        /// swbUploadName 控制項。
+        /// </summary>
+        /// <remarks>
+        /// 自動產生的欄位。
+        /// 若要修改，請將欄位宣告從設計工具檔案移到程式碼後置檔案。
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label swbUploadName;
+
+        /// <summary>
+        /// uploadfile 控制項。
+        /// </summary>
+        /// <remarks>
+        /// 自動產生的欄位。
+        /// 若要修改，請將欄位宣告從設計工具檔案移到程式碼後置檔案。
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button uploadfile;
+
+        /// <summary>
+        /// DeleteSwb 控制項。
+        /// </summary>
+        /// <remarks>
+        /// 自動產生的欄位。
+        /// 若要修改，請將欄位宣告從設計工具檔案移到程式碼後置檔案。
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button DeleteSwb;
+
+        /// <summary>
+        /// SaveButton 控制項。
+        /// </summary>
+        /// <remarks>
+        /// 自動產生的欄位。
+        /// 若要修改，請將欄位宣告從設計工具檔案移到程式碼後置檔案。
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button SaveButton;
+
+        /// <summary>
+        /// download 控制項。
+        /// </summary>
+        /// <remarks>
+        /// 自動產生的欄位。
+        /// 若要修改，請將欄位宣告從設計工具檔案移到程式碼後置檔案。
+        /// </remarks>
+        protected global::System.Web.UI.HtmlControls.HtmlAnchor download;
+
+        /// <summary>
+        /// SwbContainer 控制項。
+        /// </summary>
+        /// <remarks>
+        /// 自動產生的欄位。
+        /// 若要修改，請將欄位宣告從設計工具檔案移到程式碼後置檔案。
+        /// </remarks>
+        protected global::System.Web.UI.HtmlControls.HtmlGenericControl SwbContainer;
+    }
+}
